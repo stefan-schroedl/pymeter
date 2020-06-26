810c0e48c900810bae0feccca1428bb3a644b018
Stefan Schroedl <stefan@atomwise.com>
2020-06-25 22:20:50 -0700

    Remove requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
    name='meter',
    version='1.0',
    description='Simple utilities for stream statistics and timing',
+   install_requires=[
+       'numpy',
+       'torch',
+       'pytest'
+       ],
    author='Stefan Schroedl',
 )
